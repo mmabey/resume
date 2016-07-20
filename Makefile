mmabey/resume

commit f7156dce1d99a4315ebccbe315035eb7e9770b80

Class options: icon and iconsize, re #4
One issue I had with this was that the errors don't actually show up in
the log. The full stop definitely takes place as it should, but the .log
file doesn't contain the custom errors I wrote.

Section 4.9 of this document discusses the `\ClassError` command:
http://mirrors.concertpass.com/tex-archive/macros/latex/doc/clsguide.pdf

I tried using different packages that are wrappers for the native error
commands, but they didn't seem to help. It's possible that LaTeX3's
coffin code layer might do the trick, but it also seems complicated.
They are documented in Part XVII (~pg 177) of this document:
http://ctan.mackichan.com/macros/latex/contrib/l3kernel/source3.pdf

resume.cls
@@ -27,6 +27,13 @@
 \newtoggle{isCV}
 % font
 \newcommand\@fontselection{doesntexist}
+% icon
+\newbool{icon@tf@fn}
+\setbool{icon@tf@fn}{false}
+\def\icon@val@fn{}
+\newbool{icon@tf@sz}
+\setbool{icon@tf@sz}{false}
+\newdimen\icon@val@sz
 % pagethreshold
 \usepackage{xstring}  % Allows us to check that the parameter is an Integer
 \newcommand\@defaultPageThreshold{}
@@ -45,6 +52,12 @@
 
 % font=name
 \DeclareOptionX{font}{\renewcommand\@fontselection{#1}}
+
+% icon=filename
+\DeclareOptionX{icon}{\renewcommand{\icon@val@fn}{#1}\setbool{icon@tf@fn}{true}}
+
+% iconsize=size
+\DeclareOptionX{iconsize}{\setlength{\icon@val@sz}{#1}\setbool{icon@tf@sz}{true}}
 
 % pagethreshold=#
 \DeclareOptionX{pagethreshold}{
@@ -225,17 +238,29 @@
   \endgroup
 }
 
-\def \icon#1#2{\def\@icon{#2} \def\@iconwidth{#1}}
+\def \icon#1#2{%
+  \IfFileExists{#2}
+    {\def\icon@val@fn{#2} \setlength{\icon@val@sz}{#1} \setbool{icon@tf@fn}{true} \setbool{icon@tf@sz}{true}}
+    {\ClassError{resume}{%
+      Bad\space filename:\space #2
+    }{%
+      Please\space only\space make\space me\space try\space to\space open\space icon\space files\space that\space actually\space exist...
+    }}
+}
 
 % \printicon is used to place an icon in the top right corner
 \def \printicon {
-  \ifundef{\@icon}{}{
+  \ifboolexpr{bool{icon@tf@fn} and bool{icon@tf@sz}}{
 	\begingroup
-		\begin{wrapfigure}{r}{\@iconwidth}
+		\begin{wrapfigure}{r}{\icon@val@sz}
       \vspace{-2ex}
-			\includegraphics[width=\@iconwidth]{\@icon}
+			\includegraphics[width=\icon@val@sz]{\icon@val@fn}
 		\end{wrapfigure}~ % Without the ~, the icon goes to the end of the document
 	\endgroup
+  }{
+    \ifboolexpr{bool{icon@tf@fn} or bool{icon@tf@sz}}
+      {\ClassError{resume}{You\space must\space give\space a\space filename\space *and*\space a\space size\space to\space use\space the\space icon\space option.}
+      {If\space you\space did\space provide\space both,\space try\space making\space sure\space the\space size\space is\space a\space dimension.}}{}
   }
 }
 

commit 0c51fba95b875f693ada19bd5f291508bbc8f0be

Fixed bug from commit f7156dce, improved Makefile
It seems that the check I introduced in f7156dce is only for packages,
not arbitrary files. So since I was using it to check if a figure
actually exists before trying to use it as the icon, compiling generated
an error because of the check when it otherwise would compile just fine.

Also, the Makefile can now compile the document. Just specify the
target `cv` or `resume` and it will do the rest. (It does this via the
`latexmk` command, so that and `make` must be installed for this to
work.)

Makefile
@@ -1,8 +1,29 @@
 SHELL=/bin/bash
+COMPILE=latexmk -f -g -pdf -silent
 
 default:
-	@echo "No default target. Compile it yourself."
+	@echo "Please specify a target: cv, resume, clean"
+
+cv:
+	@echo "Compiling CV"
+	@$(COMPILE) CV_*.tex ;\
+	if [ $$? -eq 0 ] ;\
+	then echo ; echo "CV successfully compiled" ; echo ;\
+	else echo ; echo "ERRORS encountered while compiling. Check the .log file for more info." ; echo ;\
+	fi
+
+resume:
+	@echo "Compiling Resume"
+	@$(COMPILE) Resume_*.tex ;\
+	if [ $$? -eq 0 ] ;\
+	then echo ; echo "Resume successfully compiled" ; echo ;\
+	else echo ; echo "ERRORS encountered while compiling. Check the .log file for more info." ; echo ;\
+	fi
 
 clean:
 	@echo "Removing all LaTeX build files."
 	@rm -f {./,sections/,exp/}*.{aux,bbl,blg,dvi,log,fdb_latexmk,fls,out,synctex.gz,bcf,run.xml}
+
+date:
+	@echo "Updating the revision date in resume.cls to today."
+	@sed -r -i `tmpdate=\`date +%Y\\\\\\\/%m\\\\\\\/%d\`; echo "s/(^\\\\\\\\ProvidesClass\{resume\}\[)[0-9\/]{10}(.*\$$)/\1$${tmpdate}\2/;"` resume.cls

resume.cls
@@ -18,7 +18,7 @@
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-\ProvidesClass{resume}[2016/07/19 v0.9.1.4 Resume class (Mabey)]
+\ProvidesClass{resume}[2016/07/20 v0.9.1.5 Resume class (Mabey)]
 
 %%%%%%%%%%%%%%%%%%%%%%%%%% Pre-options Section - all imports and variable declarations must go here, before the first \DeclareOptionX
 \usepackage{xkeyval} % Processes keyword arguments
@@ -238,15 +238,7 @@
   \endgroup
 }
 
-\def \icon#1#2{%
-  \IfFileExists{#2}
-    {\def\icon@val@fn{#2} \setlength{\icon@val@sz}{#1} \setbool{icon@tf@fn}{true} \setbool{icon@tf@sz}{true}}
-    {\ClassError{resume}{%
-      Bad\space filename:\space #2
-    }{%
-      Please\space only\space make\space me\space try\space to\space open\space icon\space files\space that\space actually\space exist...
-    }}
-}
+\def \icon#1#2{\def\icon@val@fn{#2} \setlength{\icon@val@sz}{#1} \setbool{icon@tf@fn}{true} \setbool{icon@tf@sz}{true}}
 
 % \printicon is used to place an icon in the top right corner
 \def \printicon {